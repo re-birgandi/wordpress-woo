4f5c9c77cbdd4661c417032e4778d0a75a78a014
“re-birgandi” <greendesigner@protonmail.com>
2022-03-10 06:47:41 +0330

    edit nav html

---
+++ tpl/section-footer/footer.php
@@ -0,0 +1,92 @@
+<!-- FOOTER -->
+<footer id="footer" class="section section-grey">
+	<!-- container -->
+	<div class="container">
+		<!-- row -->
+		<div class="row">
+			<!-- footer widget -->
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<div class="footer">
+					<!-- footer logo -->
+					<div class="footer-logo">
+						<a class="logo" href="#">
+							<img src="./img/logo.png" alt="">
+						</a>
+					</div>
+					<!-- /footer logo -->
+
+					<p>این یک قالب فروشگاهی آماده جهت استفاده طراحان وب می باشد و تمام محصولات موجود در آن بعنوان نمونه طراحی است.</p>
+
+					<!-- footer social -->
+					<ul class="footer-social">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+						<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+					</ul>
+					<!-- /footer social -->
+				</div>
+			</div>
+			<!-- /footer widget -->
+
+			<!-- footer widget -->
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<div class="footer">
+
+					<img src="img/samandehi.jpg">
+				</div>
+			</div>
+			<!-- /footer widget -->
+
+			<div class="clearfix visible-sm visible-xs"></div>
+
+			<!-- footer widget -->
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<div class="footer">
+					<h3 class="footer-header">خدمات مشتری</h3>
+					<ul class="list-links">
+						<li><a href="#">درباره ما</a></li>
+						<li><a href="#">حمل و نقل و بازگشت</a></li>
+						<li><a href="#">راهنمای حمل و نقل</a></li>
+						<li><a href="#">سؤالات متداول</a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- /footer widget -->
+
+			<!-- footer subscribe -->
+			<div class="col-md-3 col-sm-6 col-xs-6">
+				<div class="footer">
+					<h3 class="footer-header">با ما بمانید</h3>
+					<p>برای اطلاع از تخفیفات و اخبار مهم فروشگاه در خبرنامه عضو شوید.</p>
+					<form>
+						<div class="form-group">
+							<input class="input" placeholder="آدرس ایمیل">
+						</div>
+						<button class="primary-btn">عضویت در خبرنامه</button>
+					</form>
+				</div>
+			</div>
+			<!-- /footer subscribe -->
+		</div>
+		<!-- /row -->
+		<hr>
+		<!-- row -->
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 text-center">
+				<!-- footer copyright -->
+				<div class="footer-copyright">
+
+					1399 &copy; تمامی حقوق این سایت متعلق به <span class="text-danger" target="_blank">مدشاپ </span>است.
+
+				</div>
+				<!-- /footer copyright -->
+			</div>
+		</div>
+		<!-- /row -->
+	</div>
+	<!-- /container -->
+</footer>
+<!-- /FOOTER -->
+
--- tpl/section-navigation/navigation.php
+++ tpl/section-navigation/navigation.php
@@ -3,7 +3,6 @@
     <!-- container -->
     <div class="container">
         <div id="responsive-nav">
-			<?php get_template_part( 'tpl/section-navigation/category', 'nav' ) ?>
 			<?php get_template_part( 'tpl/section-navigation/content', 'nav' ) ?>
         </div>
     </div>
