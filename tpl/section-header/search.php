610840484153892f8a25ca3e5763bf827913f97b
“re-birgandi” <greendesigner@protonmail.com>
2022-03-24 19:23:44 +0430

    load search box

--- tpl/section-header/search.php
+++ tpl/section-header/search.php
@@ -1,13 +1,11 @@
 <!-- Search -->
 <div class="header-search">
-	<form>
-		<input class="input search-input" type="text" placeholder="جستجو">
-		<select class="input search-categories">
-			<option value="0">همه دسته ها</option>
-			<option value="1">دسته 1</option>
-			<option value="1">دسته 2</option>
-		</select>
-		<button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
-	</form>
+    <form action="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ) ?>" method="get">
+        <input class="input search-input" type="text" name="s" placeholder="جستجو">
+		<?php
+		brs_active_plugin(BRS_PLUGIN_ACTIVE) ? brs_show_categoreis() : '';
+		?>
+        <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i></button>
+    </form>
 </div>
 <!-- /Search -->
