c3db1c5f0ecd747fe1c35f85fb87c40989f19bef
“re-birgandi” <greendesigner@protonmail.com>
2022-03-25 11:41:26 +0430

    fix search box start top_menu_navigation

--- inc/fun-actions/brs_after_setup_theme.php
+++ inc/fun-actions/brs_after_setup_theme.php
@@ -10,9 +10,14 @@ if(! function_exists('brs_after_setup_theme')){
 		add_theme_support( 'custom-header' );
 		add_theme_support( 'woocommerce' );
 		add_theme_support( 'post-thumbnails' );
+
 		register_nav_menus( array(
 			'top_menu_header' => __( 'top_menu_header', 'wordpress-woo' ),
  		) );
+		register_nav_menus( array(
+			'top_menu_navigation' => __( 'top_menu_navigation', 'wordpress-woo' ),
+		) );
+
 
 		add_image_size( "image-content-product", 270, 360 );
 
