efe7439392d471bccf98fb8fa0e3c071e6d5506f
“re-birgandi” <greendesigner@protonmail.com>
2022-03-26 07:44:28 +0430

    fix with woocommerce

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if ( ! brs_active_plugin( 'woocommerce/woocommerce.php' ) ) {
+	__( "please install plugin woocommerce", "wordpress-woo" );//translate
+
+	return;
+}
 get_header();
 
 get_footer();
--- tpl/section-navigation/content-nav.php
+++ tpl/section-navigation/content-nav.php
@@ -1,4 +1,3 @@
-
 <div id="navigation">
     <!-- container -->
     <div class="container">
@@ -8,7 +7,8 @@
                 <span class="category-header">دسته بندی ها <i class="fa fa-list"></i></span>
                 <ul class="category-list">
                     <li class="dropdown side-dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">لباس زنانه <i class="fa fa-angle-left"></i></a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">لباس زنانه <i
+                                    class="fa fa-angle-left"></i></a>
                         <div class="custom-menu">
                             <div class="row">
                                 <div class="col-md-4">
@@ -65,7 +65,9 @@
                         </div>
                     </li>
                     <li><a href="#">لباس مردانه</a></li>
-                    <li class="dropdown side-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">تلفن و لوازم جانبی <i class="fa fa-angle-left"></i></a>
+                    <li class="dropdown side-dropdown"><a class="dropdown-toggle" data-toggle="dropdown"
+                                                          aria-expanded="true">تلفن و لوازم جانبی <i
+                                    class="fa fa-angle-left"></i></a>
                         <div class="custom-menu">
                             <div class="row">
                                 <div class="col-md-4">
@@ -129,7 +131,8 @@
                     <li><a href="#">رایانه و اداری</a></li>
                     <li><a href="#">الکترونیک</a></li>
                     <li class="dropdown side-dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">ساعت و جواهرات <i class="fa fa-angle-left"></i></a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">ساعت و جواهرات <i
+                                    class="fa fa-angle-left"></i></a>
                         <div class="custom-menu">
                             <div class="row">
                                 <div class="col-md-4">
@@ -215,165 +218,15 @@
             <!-- menu nav -->
             <div class="menu-nav">
                 <span class="menu-header">منو <i class="fa fa-bars"></i></span>
-                <ul class="menu-list">
-                    <li><a href="#">صفحه اصلی</a></li>
-                    <li><a href="#">فروشگاه</a></li>
-                    <li class="dropdown mega-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">زنانه <i class="fa fa-caret-down"></i></a>
-                        <div class="custom-menu">
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                    <hr class="hidden-md hidden-lg">
-                                </div>
-                                <div class="col-md-4">
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                    <hr class="hidden-md hidden-lg">
-                                </div>
-                                <div class="col-md-4">
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="row hidden-sm hidden-xs">
-                                <div class="col-md-12">
-                                    <hr>
-                                    <a class="banner banner-1" href="#">
-                                        <img src="./img/banner05.jpg" alt="">
-                                        <div class="banner-caption text-center">
-                                            <h2 class="white-color">مجموعه جدید</h2>
-                                            <h3 class="white-color font-weak">پیشنهاد ویژه</h3>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="dropdown mega-dropdown full-width"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">مردانه <i class="fa fa-caret-down"></i></a>
-                        <div class="custom-menu">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="./img/banner06.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">زنانه</h3>
-                                            </div>
-                                        </a>
-                                        <hr>
-                                    </div>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="./img/banner07.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">کیف</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="./img/banner08.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">لوازم جانبی</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                </div>
-                                <div class="col-md-3">
-                                    <div class="hidden-sm hidden-xs">
-                                        <a class="banner banner-1" href="#">
-                                            <img src="./img/banner09.jpg" alt="">
-                                            <div class="banner-caption text-center">
-                                                <h3 class="white-color text-uppercase">مردانه</h3>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <hr>
-                                    <ul class="list-links">
-                                        <li>
-                                            <h3 class="list-links-title">دسته بندی ها</h3>
-                                        </li>
-                                        <li><a href="#">لباس زنانه</a></li>
-                                        <li><a href="#">لباس مردانه</a></li>
-                                        <li><a href="#">تلفن و لوازم جانبی</a></li>
-                                        <li><a href="#">جواهرات و ساعتها</a></li>
-                                        <li><a href="#">کیف و کفش</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li class="dropdown default-dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">صفحات <i class="fa fa-caret-down"></i></a>
-                        <ul class="custom-menu">
-                            <li><a href="index.html">صفحه اصلی</a></li>
-                            <li><a href="products.html">محصولات</a></li>
-                            <li><a href="product-page.html">جزئیات محصولات</a></li>
-                            <li><a href="checkout.html">تسویه حساب</a></li>
-                        </ul>
-                    </li>
-                </ul>
+				<?php
+				wp_nav_menu( [
+					'theme_location' => 'top_menu_navigation',
+					'container'      => 'ul',
+					'menu_class'     => 'menu-list',
+					'menu_id'        => false,
+					'walker'         => brs_active_plugin( BRS_PLUGIN_ACTIVE ) ? new Brs_Menu_Navigation : ' '
+				] )
+				?>
             </div>
             <!-- menu nav -->
         </div>
